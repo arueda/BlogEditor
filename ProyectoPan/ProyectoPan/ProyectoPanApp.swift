cd68cf9a027f4be82fc000e1efa36efc8fed3bfb
Mairym Ximena Rueda Ciprés <ximena@angels-air.home>
2020-09-26 19:49:39 -0700

    cree el proyecto

---
+++ ProyectoPan/ProyectoPan/ProyectoPanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoPanApp.swift
+//  ProyectoPan
+//
+//  Created by Mairym Ximena Rueda Ciprés on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoPanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
